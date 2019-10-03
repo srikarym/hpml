1044b7b61a63a4a20c89e5e810a2ee6478f1c40a
srikarym <msy290@nyu.edu>
2019-10-03 14:06:08 -0400

    q3

---
+++ hw1/dp3.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include <mkl_cblas.h>
+
+#define BILLION 1E9
+
+struct timespec start, end;
+
+double bdp(long N, float *pA, float *pB) {
+	double R = cblas_sdot(N, pA, 1, pB, 1);
+	return R;
+}
+
+
+
+int main(int argc, char* argv[])
+{
+	long N = atoi(argv[1]);
+	int repetitions = atoi(argv[2]);
+
+	long FLOP = 2L * N;
+
+	float pa[N];
+	float pb[N];
+	double ans=0.0;
+	int i = 0;
+	double avg_time = 0.0;
+
+	for (i=0; i<N; i++)
+	{
+		pa[i] = 1.0;
+		pb[i] = 1.0;	
+	}
+
+	for (i=0; i<repetitions; i++)
+	{
+		clock_gettime(CLOCK_MONOTONIC, &start);
+		ans = bdp(N, pa, pb);
+		
+		assert(ans == (double)N);
+		clock_gettime(CLOCK_MONOTONIC, &end);
+
+		if (i > repetitions/2)
+		{
+			double time_1 = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / BILLION;
+			avg_time += time_1;
+		}
+	}
+
+	avg_time = avg_time * 2 / (repetitions);
+
+	double bw = (double)N * (double) sizeof(double) / (avg_time * BILLION);
+	double flops = (double) FLOP / avg_time;
+	
+	printf("N: %ld\n<T>: %lf sec \nBw: %lf GB/s \nF: %lf FLOP/s\n",N,avg_time ,bw, flops);
+	return 0;
+}
